58f4fb4bc3da31e48dbcd65f8e528708621c0195
Vaughan Sampson <128713660+VaughanSampson@users.noreply.github.com>
2023-10-04 10:42:26 +1000

    Add Position Javadocs

--- src/models/Position.java
+++ src/models/Position.java
@@ -1,25 +1,25 @@
 package models;
 
 /**
- * Stores an (x, y) position that can be retrieved.
+ * Stores an (x, y) position.
  */
 public class Position {
 
+    /** An axis component of the position. */
     private final int x, y;
 
     /**
      * Constructs position.
-     * @param x X-axis position value.
-     * @param y Y-axis position value.
+     * @param x X-axis component of position.
+     * @param y Y-axis component of position.
      */
     public Position(int x, int y) {
-
         this.x = x;
         this.y = y;
     }
 
     /**
-     * Gets X-axis position.
+     * Gets the X-axis component of the position.
      * @return x.
      */
     public int getX() {
@@ -27,25 +27,43 @@ public class Position {
     }
 
     /**
-     * Gets Y-axis position.
+     * Gets the Y-axis component of the position.
      * @return y.
      */
     public int getY() {
         return y;
     }
 
+    /**
+     * Compares this position with another object for equality.
+     * @param obj Other object.
+     * @return True if the object is a position with the same x and
+     * y components. Otherwise, false.
+     */
     @Override
     public boolean equals(Object obj) {
-        if(obj instanceof Position)
+        if(obj instanceof Position) {
             return (x == ((Position) obj).getX() && y == ((Position) obj).getY());
-        else
+        }
+        else {
             return false;
+        }
     }
 
-    public Position add(Position pos){
-        return new Position(this.x + pos.getX(), this.y + pos.getY());
+    /**
+     * Create a position which is the vector addition of this position and
+     * another position.
+     * @param otherPosition Position to add to this position.
+     * @return The position resulting from the addition.
+     */
+    public Position add(Position otherPosition){
+        return new Position(this.x + otherPosition.getX(), this.y + otherPosition.getY());
     }
 
+    /**
+     * Get the string representation of the position.
+     * @return The string representation of the position.
+     */
     @Override
     public String toString() {
         return "Position (" + x + ", " + y + ")";
