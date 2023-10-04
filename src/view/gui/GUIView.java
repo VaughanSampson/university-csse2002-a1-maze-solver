455025742cdc66810e6c5e9c2b51b591a4aaade0
Vaughan Sampson <128713660+VaughanSampson@users.noreply.github.com>
2023-10-04 11:01:31 +1000

    Add Javadocs to GUIView

--- src/view/gui/GUIView.java
+++ src/view/gui/GUIView.java
@@ -6,38 +6,53 @@ import view.View;
 import java.awt.*;
 import javax.swing.*;
 
+/**
+ * Creates and initiates a window GUI view of a Maze.
+ */
 public class GUIView implements View {
 
+    /** Frame of display. */
     private final JFrame frame;
 
+    /**
+     * Constructor initiating the frame of display.
+     */
     public GUIView(){
         frame = new JFrame("Maze Solver");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1200,675);
         frame.setResizable(false);
-        frame.setVisible(true);
     }
 
+    /**
+     * Generates the maze display onto the frame and makes the frame visible.
+     * @param maze Maze to display.
+     * @param message Message to show within display.
+     */
     @Override
     public void generateDisplay(Maze maze, String message){
+
+        // If there is a maze, create a maze display panel, otherwise make an
+        // empty panel.
         JPanel panel;
+
         if(maze != null) {
             panel = new GUIMazePanel(frame.getWidth(), frame.getHeight(), maze);
         }
         else
         {
-            // Create empty panel
             panel = new JPanel();
         }
 
-        // Put text into panel
+        // Put a message into the panel
         panel.setBorder(BorderFactory.createEmptyBorder(20,0,0,0));
         JLabel label = new JLabel(message);
         label.setFont(new Font("Arial", Font.PLAIN, 30));
         panel.add(label);
 
-        // Render panel
+        // Render everything
         frame.add(panel);
+        frame.setVisible(true);
     }
 
 }
