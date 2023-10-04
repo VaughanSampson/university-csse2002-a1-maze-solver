613dd5d56784b10e204de8da6829f791e74a27cd
Vaughan Sampson <128713660+VaughanSampson@users.noreply.github.com>
2023-10-04 11:05:59 +1000

    Add Launcher Javadocs

--- src/Launcher.java
+++ src/Launcher.java
@@ -2,12 +2,21 @@ import  controllers.MainController;
 
 import java.io.File;
 
+/**
+ * Initiates the program.
+ */
 public class Launcher {
+
+    /**
+     * Initiates the program utilising program arguments to define
+     * functionality.
+     * @param args Program arguments.
+     */
     public static void main(String[] args) {
 
+        // Gets useful data from program arguments
         String mazePath = null;
         boolean usesGUI = false;
-
         for(String s : args){
             if(s.equals("GUI")) {
                 usesGUI = true;
@@ -19,14 +28,16 @@ public class Launcher {
             }
         }
 
+        // Throws exception if there was no valid maze file path program argument
         if(mazePath == null) {
-            throw new IllegalArgumentException("A maze file's name ending in '.txt' must be entered as a program " +
-                    "argument on compile.");
+            throw new IllegalArgumentException("A maze file's name ending in " +
+                    "'.txt' must be entered as a program argument on compile.");
         }
 
-
+        // Creates complete path to maze file to load
         String filePath = new File("").getAbsolutePath()+"\\mazes\\"+mazePath;
 
+        // Calls the MainController to load, solve and display the maze
         MainController.solveAndDisplayMaze(filePath, usesGUI);
     }
 }
